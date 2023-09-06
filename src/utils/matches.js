0856f1f7d63289762eafcc5f9e51417ae4089229
Gabriel Gelado <gbgelado@gmail.com>
2023-09-06 10:21:24 -0300

    fixes

--- src/utils/matches.js
+++ src/utils/matches.js
@@ -7,23 +7,16 @@ export default {
     min = parseInt(String(min).substr(0, substr.length), 10);
     max = parseInt(String(max).substr(0, substr.length), 10);
 
-    return (
-      integerRepresentationOfCardNumber >= min &&
-      integerRepresentationOfCardNumber <= max
-    );
+    return integerRepresentationOfCardNumber >= min && integerRepresentationOfCardNumber <= max;
   },
 
   matchesPattern(cardNumber, pattern) {
     pattern = String(pattern);
-    return (
-      pattern.substring(0, cardNumber.length) ===
-      cardNumber.substring(0, pattern.length)
-    );
+    return pattern.substring(0, cardNumber.length) === cardNumber.substring(0, pattern.length);
   },
 
   matches(cardNumber, pattern) {
-    if (Array.isArray(pattern))
-      return this.matchesRange(cardNumber, pattern[0], pattern[1]);
+    if (Array.isArray(pattern)) return this.matchesRange(cardNumber, pattern[0], pattern[1]);
     return this.matchesPattern(cardNumber, pattern);
   },
 };
